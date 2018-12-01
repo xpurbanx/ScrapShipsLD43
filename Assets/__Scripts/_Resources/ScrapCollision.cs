ab4f74489cb3dd778a62929c042c213341c12aac
Sharpyfile <44209792+Sharpyfile@users.noreply.github.com>
2018-12-01 13:12:47 +0100

    Zbieranie złomu

---
+++ Assets/__Scripts/_Resources/ScrapCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrapCollision : MonoBehaviour
+{
+   
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Scrap")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
