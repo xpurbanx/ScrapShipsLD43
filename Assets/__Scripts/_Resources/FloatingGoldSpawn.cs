c0ff93b5c51687c33bc4eabe00af710bbed9ce1b
Sharpyfile <44209792+Sharpyfile@users.noreply.github.com>
2018-12-01 13:36:31 +0100

    Dodanie złota

---
+++ Assets/__Scripts/_Resources/FloatingGoldSpawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingGoldSpawn : MonoBehaviour {
+
+    public GameObject prefab;
+    private float lastSpawn = 0;
+    public float cooldownTime = 5;
+		
+	void Update ()
+    {
+        if (Time.time >= lastSpawn + cooldownTime)
+        {
+            Instantiate(prefab, new Vector2(Random.Range(-9, 5), 9.9f), Quaternion.identity);
+            lastSpawn = Time.time;
+        }
+          
+    }
+}
