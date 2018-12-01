6e2507efb31f04a6c1fb8cb3683b097cb6a3a952
Purban <xpurbanx@wp.pl>
2018-12-01 09:40:30 +0100

    Commit

---
+++ Assets/__Scripts/_Player/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public int playerNumber = 1;
+    public string inputHorizontal, inputVertical;
+
+    private void Awake()
+    {
+        
+    }
+    void Start () {
+
+        if (playerNumber == 1)
+        {
+            inputHorizontal = "Horizontal1";
+            inputVertical = "Vertical1";
+        }
+
+        else if (playerNumber == 2)
+        {
+            inputHorizontal = "Horizontal2";
+            inputVertical = "Vertical2";
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/__Scripts/_Player/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Player {
+
+    private Rigidbody2D rigibody;
+    public float movementSpeed = 10f;
+    public float rotateSpeed = 100f;
+    private float moveDirection;
+    private float moveAmount;
+    private float rotateAmount;
+    private Transform transform;
+    private Player player;
+
+    private float moveRotation;
+    
+
+	void Start ()
+    {
+        player.GetComponent<Player>();
+        rigibody = GetComponent<Rigidbody2D>();
+        transform = GetComponent<Transform>();
+	}
+
+    private void Update()
+    {
+
+        Movement();
+
+    }
+    // Update is called once per frame
+    //void FixedUpdate ()
+    //{
+    //    if (Input.GetAxisRaw("Vertical"))
+    //        rigibody.MovePosition(new Vector2(rigibody.transform.position.x, rigibody.transform.position.y + 1*movementSpeed));
+
+
+    //    if (Input.GetKey(KeyCode.DownArrow))
+    //        rigibody.MovePosition(new Vector2(rigibody.transform.position.x, rigibody.transform.position.y + -1*movementSpeed));
+
+
+    //    if (Input.GetKey(KeyCode.LeftArrow))
+    //        rigibody.MovePosition(new Vector2(rigibody.transform.position.x - 1*movementSpeed, rigibody.transform.position.y));
+
+
+    //    if (Input.GetKey(KeyCode.RightArrow))
+    //        rigibody.MovePosition(new Vector2(rigibody.transform.position.x + 1*movementSpeed, rigibody.transform.position.y));
+
+    //}
+    private void Movement()
+    {
+        moveDirection = Input.GetAxisRaw(player.inputHorizontal);
+        moveAmount = moveDirection * movementSpeed * Time.deltaTime;
+        transform.position += transform.up * moveAmount;
+
+        moveRotation = Input.GetAxisRaw(player.inputVertical);
+        rotateAmount = moveRotation * rotateSpeed * Time.deltaTime;
+        transform.Rotate(new Vector3(0, 0 , -rotateAmount));
+    }
+}
