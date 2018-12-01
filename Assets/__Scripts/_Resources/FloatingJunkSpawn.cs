94cd1dd7dd22afbb35f0377182ffcd27d349bc46
Sharpyfile <44209792+Sharpyfile@users.noreply.github.com>
2018-12-01 11:44:03 +0100

    Wrzucenie skryptów do scrapa

---
+++ Assets/__Scripts/_Resources/FloatingJunkSpawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingJunkSpawn : MonoBehaviour {
+
+    public GameObject prefab;
+    private float lastSpawn = 0;
+    public float cooldownTime = 5;
+		
+	void Update ()
+    {
+        if (Time.time >= lastSpawn + cooldownTime)
+        {
+            Instantiate(prefab, new Vector2(Random.Range(-9, 9), 5.9f), Quaternion.identity);
+            lastSpawn = Time.time;
+        }
+          
+    }
+}
---
+++ Assets/__Scripts/_Resources/FloatingScrapMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingScrapMovement : MonoBehaviour {
+
+    public Transform prefab;
+    public GameObject gameObjectWhenUseless;
+    public float scrapSpeed = 1f;
+
+	void Start ()
+    {
+        prefab = GetComponent<Transform>();
+	}
+	
+	
+	void FixedUpdate ()
+    {
+        if (prefab.position.y < 6)
+            prefab.position -= new Vector3(0, Time.deltaTime*scrapSpeed, 0);
+        if (prefab.position.y < - 10)
+            Destroy(gameObjectWhenUseless);
+
+    }
+
+    
+        
+    
+}
