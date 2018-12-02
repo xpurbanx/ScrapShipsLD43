1c884593bd8b796d3bd2cfb191ce670660adcd8b
Purban <xpurbanx@wp.pl>
2018-12-02 11:47:20 +0100

    Alert on cannons

---
+++ Assets/__Scripts/PlayerCommon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCommon : MonoBehaviour {
+    public static PlayerCommon Instance;
+
+
+    public Sprite alert1, alert2, alert3;
+    // Use this for initialization
+    void Start () {
+        Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
