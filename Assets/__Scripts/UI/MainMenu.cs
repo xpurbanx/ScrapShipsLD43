f1a5d2fe102ff2258cbb7be6463215258e8d545a
Sharpyfile <44209792+Sharpyfile@users.noreply.github.com>
2018-12-02 09:49:06 +0100

    Menu

---
+++ Assets/__Scripts/UI/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Ładowanie Sceny");
+        SceneManager.LoadScene("ScenaTestowa");
+    }
+	
+    public void ExitGame()
+    {
+        Debug.Log("Wyłączenie aplikacji");
+        Application.Quit();
+    }
+
+}
